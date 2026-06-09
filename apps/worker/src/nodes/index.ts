30bdaca509ee7a62a23cd715c793b4b480fad0a0
DrozsdikAdam <drozsdik17@gmail.com>
2026-06-09 19:35:19 +0200

    adding llm s  and their handlings

--- apps/worker/src/nodes/index.ts
+++ apps/worker/src/nodes/index.ts
@@ -1,4 +1,5 @@
 import { httpHandler } from "./httpHandler";
+import { llmHandler } from "./llmHandler";
 
 type NodeHandler = (data: Record<string, any>, context: Record<string, any>, userId: string) => Promise<any>;
 
@@ -9,9 +10,7 @@ export const nodeHandler = (type: string): NodeHandler => {
                     return data;
                }
           case "llm":
-               return async (data, context, options) => {
-                    return { output: "mock még nincs implementálva!" }
-               }
+               return llmHandler;
 
           case "http":
                return httpHandler;
