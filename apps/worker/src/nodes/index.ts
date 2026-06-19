8bdb6546cdbdebc975f223eda7715dd57854195f
DrozsdikAdam <drozsdik17@gmail.com>
2026-06-19 18:32:06 +0200

    adding logic node handler

--- apps/worker/src/nodes/index.ts
+++ apps/worker/src/nodes/index.ts
@@ -1,5 +1,6 @@
 import { httpHandler } from "./httpHandler";
 import { llmHandler } from "./llmHandler";
+import { logicHandler } from "./logicHandler";
 
 type NodeHandler = (data: Record<string, any>, context: Record<string, any>, userId: string) => Promise<any>;
 
@@ -17,7 +18,7 @@ export const nodeHandler = (type: string): NodeHandler => {
 
           case "logic":
                return async (data, context, options) => {
-                    return { output: "mock még nincs implementálva!" }
+                    return logicHandler(data, context, options)
                }
 
           default: throw new Error(`Unknown node type: ${type}`)
