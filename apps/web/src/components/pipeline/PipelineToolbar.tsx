ce1f56c3c0e68a2e6008521f255f83e6cb2ded96
DrozsdikAdam <drozsdik17@gmail.com>
2026-06-24 15:47:27 +0200

    adding node editor changes to loacal data storing

--- apps/web/src/components/pipeline/PipelineToolbar.tsx
+++ apps/web/src/components/pipeline/PipelineToolbar.tsx
@@ -61,21 +61,22 @@ export default function PipelineToolbar() {
             <div className="flex items-center gap-4">
                 <input className="text-lg border-0 rounded p-2 outline-none hover:outline hover:outline-gray-300" value={name} onChange={(e) => setName(e.target.value)} />
             </div>
-            <div className="flex items-center gap-4">
-                <div className="w-full">
-                    <p className={executionError ?
-                        "text-red-500" : executionStatus === "running" ?
-                            "text-blue-500" : executionStatus === "completed" ?
-                                "text-green-500" : "text-gray-500"}>
 
-                        {executionError ? executionError :
-                            executionStatus === "saving" ? "Mentés..." :
-                                executionStatus === "running" ? "Futtatás folyamatban..." :
-                                    executionStatus === "completed" ? "Sikeres futtatás!" :
-                                        executionStatus === "failed" ? "Hiba történt" :
-                                            ""}
-                    </p>
-                </div>
+            <div className="w-full">
+                <p className={executionError ?
+                    "text-red-500" : executionStatus === "running" ?
+                        "text-blue-500" : executionStatus === "completed" ?
+                            "text-green-500" : "text-gray-500"}>
+
+                    {executionError ? executionError :
+                        executionStatus === "saving" ? "Mentés..." :
+                            executionStatus === "running" ? "Futtatás folyamatban..." :
+                                executionStatus === "completed" ? "Sikeres futtatás!" :
+                                    executionStatus === "failed" ? "Hiba történt" :
+                                        ""}
+                </p>
+            </div>
+            <div className="flex items-center gap-4">
                 <button className="text-md bg-green-600 text-white rounded px-4 py-2 hover:bg-green-700 transition-colors" disabled={isLoading} onClick={() => handleSave(id)}>{isLoading ? <Loader2 size={20} className="animate-spin text-white" /> : "Mentés"}</button>
                 <button className="text-md bg-blue-600 text-white rounded px-4 py-2 hover:bg-blue-700 transition-colors" disabled={isLoading} onClick={() => handleExecute(id)}>{isLoading ? <Loader2 size={20} className="animate-spin text-white" /> : "Futtatás"}</button>
                 <button className="text-md bg-red-600 text-white rounded px-4 py-2 hover:bg-red-700 transition-colors" disabled={isLoading} onClick={() => handleDelete(id)}>{isLoading ? <Loader2 size={20} className="animate-spin text-white" /> : "Törlés"}</button>
